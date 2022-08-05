2acfb9a5c13d8836f0640a4a5f0ade5f4f5eb3f1
rajnikantkori395 <109276424+rajnikantkori395@users.noreply.github.com>
2022-08-05 14:49:16 +0530

    JS program

---
+++ maxmin.js
@@ -0,0 +1,10 @@
+function sumMaxMin(a, b, c, d, e) {
+
+   // const arr = [a, b, c, d, e];
+
+    return Math.max(a,b,c,d,e) + Math.min(a,b,c,d,e);
+}
+
+console.log(sumMaxMin(100, 100, -200, 300, 0));
+console.log(sumMaxMin(1, 3, 2, 4, 5));
+console.log(sumMaxMin(-1000, -2000, -10, -120, -60));
